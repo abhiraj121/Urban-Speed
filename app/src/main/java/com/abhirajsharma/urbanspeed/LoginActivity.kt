4b756541e5eacf22c8be03358dfa659476a2e9f7
Abhiraj Sharma <abhi9899akshansh@gmail.com>
2020-09-09 09:12:49 +0530

    Login with phone and google all completed.

--- app/src/main/java/com/abhirajsharma/urbanspeed/LoginActivity.kt
+++ app/src/main/java/com/abhirajsharma/urbanspeed/LoginActivity.kt
@@ -2,9 +2,7 @@ package com.abhirajsharma.urbanspeed
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -21,20 +19,14 @@ import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
 import kotlinx.android.synthetic.main.activity_login_actvity.*
 import java.util.concurrent.TimeUnit
 
-
 class LoginActivity : AppCompatActivity() {
 
-    val TAG = "creationOfObject"
+    lateinit var mCallbacks:PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
     private val auth by lazy { FirebaseAuth.getInstance() }
-    lateinit var mVerificationId:String
-//    lateinit var mResendToken
-
-//    private val TAG = "GoogleActivity"
+    var verificationId:String = ""
     private val RC_SIGN_IN = 9001
-//    private lateinit var auth: FirebaseAuth
-    private lateinit var signOut: Button
-    private lateinit var GoogleSignInClient: GoogleSignInClient
-
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,17 +34,32 @@ class LoginActivity : AppCompatActivity() {
 
         otpBtn.setOnClickListener {
             progressBar.visibility = View.VISIBLE
+            verify()
+        }
 
-            val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        verifyBtn.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+            authenticate()
+        }
 
-                override fun onVerificationCompleted(credential: PhoneAuthCredential) {
-                    Log.d(TAG, "onVerificationCompleted:$credential")
-                    signInWithPhoneAuthCredential(credential)
-                }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        google_Login.setOnClickListener {
+            signIn()
+        }
+    }
 
-                override fun onVerificationFailed(e: FirebaseException) {
-                    progressBar.visibility = View.GONE
-                    Log.w(TAG, "onVerificationFailed", e)
+    private fun verificationCallbacks(){
+        mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                signInWithPhoneAuthCredential(p0)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                progressBar.visibility = View.GONE
                     if (e is FirebaseAuthInvalidCredentialsException) {
                         if (phone_et.text?.isEmpty()!!){
                             rootLayout.showSnackBar("The field cannot be empty.")
@@ -62,53 +69,49 @@ class LoginActivity : AppCompatActivity() {
                     } else if (e is FirebaseTooManyRequestsException) {
                         rootLayout.showSnackBar(e.localizedMessage!!.toString())
                     }
-
-                }
-
-                override fun onCodeSent(verificationId: String, token: ForceResendingToken) {
-                    Log.d(TAG, "onCodeSent:$verificationId")
-                    mVerificationId = verificationId
-//                    mResendToken = token
-                }
-
             }
-            PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                    "+91" + phone_et.text.toString(),
-                    60,
-                    TimeUnit.SECONDS,
-                    this,
-                    callbacks
-            )
-        }
 
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
-//        signOut = findViewById<View>(R.id.signOutBtn) as Button
-        GoogleSignInClient = GoogleSignIn.getClient(this, gso)
-        google_Login.setOnClickListener {
-            signIn()
+            override fun onCodeSent(p0: String, p1: ForceResendingToken) {
+                super.onCodeSent(p0, p1)
+                verificationId = p0
+                progressBar.visibility = View.GONE
+            }
         }
+    }
+
+    private fun authenticate() {
+        val verificationNum = otp_et.text.toString()
+        val credential = PhoneAuthProvider.getCredential(verificationId, verificationNum)
+        signInWithPhoneAuthCredential(credential)
+    }
+
+    private fun verify() {
+        verificationCallbacks()
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91"+phone_et.text.toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks)
+
     }
 
     private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
         auth.signInWithCredential(credential).addOnCompleteListener(this) {
-            Log.d("TAG", "signInWithCredential:success")
-            rootLayout.showSnackBar("Account Created Successfully")
-            progressBar.visibility = View.GONE
-            startActivity(Intent(this, MainActivity::class.java))
+            if (it.isSuccessful){
+                rootLayout.showSnackBar("Account Created Successfully.")
+                progressBar.visibility = View.GONE
+                startActivity(Intent(this, MainActivity::class.java))
+            }
         }.addOnFailureListener {
             rootLayout.showSnackBar("Error : ${it.localizedMessage}")
         }
     }
 
     private fun signIn () {
-        val signInIntent = GoogleSignInClient.signInIntent
+        val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIGN_IN) {
@@ -119,38 +122,27 @@ class LoginActivity : AppCompatActivity() {
         }
     }
     private fun handleResult (completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account= completedTask.getResult(ApiException::class.java)!!
-            firebaseAuthWithGoogle(account.idToken!!)
+        val account= completedTask.getResult(ApiException::class.java)!!
+        firebaseAuthWithGoogle(account.idToken!!)
 //            updateUI (account)
-        } catch (e: ApiException) {
-//            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
-        }
-    }
 
+    }
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "signInWithCredential:success")
                         val user = auth.currentUser!!
                         updateUI(user)
                     } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "signInWithCredential:failure", task.exception)
                         rootLayout.showSnackBar("Authentication Failed.")
-//                        updateUI(null)
                     }
                 }
     }
-
     private fun updateUI (account: FirebaseUser) {
         progressBar.visibility = View.GONE
         startActivity(Intent(this, MainActivity::class.java))
     }
-
     private fun View.showSnackBar(msg: String) {
         val snack = Snackbar.make(this, msg, Snackbar.LENGTH_SHORT)
         snack.animationMode = Snackbar.ANIMATION_MODE_SLIDE
